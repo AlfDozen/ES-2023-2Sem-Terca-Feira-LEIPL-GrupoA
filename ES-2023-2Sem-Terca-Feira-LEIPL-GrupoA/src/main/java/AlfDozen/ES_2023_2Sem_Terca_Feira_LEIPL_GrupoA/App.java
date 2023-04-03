ee10cc3c91c85633c7ebf2263a0c48d72a485cf6
pmaaa2-iscte <pmaaa2@iscte-iul.pt>
2023-04-03 12:05:42 +0100

    first commit

---
+++ ES-2023-2Sem-Terca-Feira-LEIPL-GrupoA/src/main/java/AlfDozen/ES_2023_2Sem_Terca_Feira_LEIPL_GrupoA/App.java
@@ -0,0 +1,13 @@
+package AlfDozen.ES_2023_2Sem_Terca_Feira_LEIPL_GrupoA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
