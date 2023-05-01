bedea700894707f59c82b6e4650445c2de4f0cf2
afarl-iscteiul <alexander_ferreira@iscte-iul.pt>
2023-05-01 11:21:05 +0100

    1 - Metodo overlaps() criado na clase TimeSlot

--- ES-2023-2Sem-Terca-Feira-LEIPL-GrupoA/src/main/java/alfdozen/es_2023_2sem_terca_teira_leipl_grupoa/TimeSlot.java
+++ ES-2023-2Sem-Terca-Feira-LEIPL-GrupoA/src/main/java/alfdozen/es_2023_2sem_terca_teira_leipl_grupoa/TimeSlot.java
@@ -443,4 +443,22 @@ final class TimeSlot implements Comparable<TimeSlot> {
 	public String toString() {
 		return getDateString() + " - " + getTimeBeginString() + "-" + getTimeEndString();
 	}
+	
+	/**
+	 * Verify if this TimeSlot overlaps a given TimeSlot by
+	 * checking if this timeslots timeBegin is before the timeEnd given TimeSlot or
+	 * this TimeSlot timeEnd is after the timeBegin of the given TimeSlot
+	 * @param other The other TimeSlot to be verified.
+	 * @return true is overlaps, false otherwise.
+	 */
+	public boolean overlaps(TimeSlot other) {
+	    if (other == null || !other.isComplete() || this == null || !this.isComplete()) {
+	        return false;
+	    }
+	    if (other.date.equals(this.date))
+	    	return(this.timeBegin.compareTo(other.timeEnd)<0) && (this.timeEnd.compareTo(other.timeBegin)>0);
+	    else
+	    	return false;
+	}
+
 }
