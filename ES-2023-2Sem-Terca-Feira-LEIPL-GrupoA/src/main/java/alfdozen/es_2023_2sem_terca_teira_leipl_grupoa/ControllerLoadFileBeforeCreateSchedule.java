ffbe0d34f628668141f87752f575b6acd687cc1b
carfa2-iscte <alexandra_rocha@iscte-iul.pt>
2023-05-06 17:28:05 +0100

    2 - Implementar interface
    Implementação da interface para carregar o ficheiro antes da criação do
    horário

--- ES-2023-2Sem-Terca-Feira-LEIPL-GrupoA/src/main/java/alfdozen/es_2023_2sem_terca_teira_leipl_grupoa/ControllerLoadFileBeforeCreateSchedule.java
+++ ES-2023-2Sem-Terca-Feira-LEIPL-GrupoA/src/main/java/alfdozen/es_2023_2sem_terca_teira_leipl_grupoa/ControllerLoadFileBeforeCreateSchedule.java
@@ -1,6 +1,5 @@
 package alfdozen.es_2023_2sem_terca_teira_leipl_grupoa;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -14,108 +13,197 @@ import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
-//PARA APAGAR: CLASSE DE PAGINA DE IR BUSCAR HORÁRIO AO CSV, JSON OU FICHEIRO NA NET
 public class ControllerLoadFileBeforeCreateSchedule implements Initializable{
-
+	
+	static final String TYPE_MESSAGE = "Alerta";
 
 	@FXML
-	private Button saveFileCSV, saveFileJSON, backButton, getFile, viewSchedule, uploadFileCSV, uploadFileJSON;
-
+	private Button backButton;
+	@FXML
+	private Button createScheduleButton;
+	@FXML
+	private Button uploadFileButton;
+	@FXML
+	private Button getFileButton;
+	@FXML
+	private RadioButton optionLocal = new RadioButton();
+	@FXML
+	private RadioButton optionOnline = new RadioButton();
+	@FXML
+	private RadioButton optionFenix = new RadioButton();
+	@FXML
+	private RadioButton optionPrevious = new RadioButton();
 	@FXML
 	private Label fileChosen;
-	private FileChooser fileChooser  = new FileChooser();
-	private File filePath, filePathToSave;
-	private String extension, filename, filenameToSave;
-	private Schedule scheduleUploaded = null;
+	@FXML
+	private Label labelOnline;
+	@FXML
+	private Label labelFenix;
+	@FXML
+	private Label labelPreviousLoad;
+	@FXML
+	private Label labelPreviousNotLoad;
+	@FXML
+	private TextField urlChosen;
+	@FXML
+	private TextField webcalChosen;
 
 	@FXML
 	private AnchorPane window; 
-
+	
 	@FXML
-	private void viewSchedule() throws IOException {
-		App.setRoot("/fxml/ViewSchedule");
+	private void setLocal() {
+		optionLocal.setSelected(true);
+		fileChosen.setText("Nenhum ficheiro importado");
+		fileChosen.setVisible(true);
+		getFileButton.setVisible(true);
+		
+		labelOnline.setVisible(false);
+		urlChosen.setVisible(false);
+		labelFenix.setVisible(false);
+		webcalChosen.setVisible(false);
+		labelPreviousLoad.setVisible(false);
+		labelPreviousNotLoad.setVisible(false);
+		
+		uploadFileButton.setVisible(false);
+		createScheduleButton.setVisible(false);
 	}
-
+	
+	@FXML
+	private void setOnline() {
+		optionOnline.setSelected(true);
+		labelOnline.setVisible(true);
+		urlChosen.setText("");
+		urlChosen.setVisible(true);
+		
+		fileChosen.setVisible(false);
+		getFileButton.setVisible(false);
+		labelFenix.setVisible(false);
+		webcalChosen.setVisible(false);
+		labelPreviousLoad.setVisible(false);
+		labelPreviousNotLoad.setVisible(false);
+		
+		uploadFileButton.setVisible(false);
+		createScheduleButton.setVisible(false);
+	}
+	
+	@FXML
+	private void setFenix() {
+		optionFenix.setSelected(true);
+		labelFenix.setVisible(true);
+		webcalChosen.setVisible(true);
+		webcalChosen.setText("");
+		
+		fileChosen.setVisible(false);
+		getFileButton.setVisible(false);
+		labelOnline.setVisible(false);
+		urlChosen.setVisible(false);
+		labelPreviousLoad.setVisible(false);
+		labelPreviousNotLoad.setVisible(false);
+		
+		uploadFileButton.setVisible(false);
+		createScheduleButton.setVisible(false);
+	}
+	
+	@FXML
+	private void setPrevious() {
+		optionPrevious.setSelected(true);
+		
+		fileChosen.setVisible(false);
+		getFileButton.setVisible(false);
+		labelOnline.setVisible(false);
+		urlChosen.setVisible(false);
+		labelFenix.setVisible(false);
+		webcalChosen.setVisible(false);
+		
+		uploadFileButton.setVisible(false);
+		
+		if (scheduleIsEmpty(App.SCHEDULE)) {
+			labelPreviousLoad.setVisible(false);
+			labelPreviousNotLoad.setVisible(true);
+			createScheduleButton.setVisible(false);
+		} else {
+			labelPreviousLoad.setVisible(true);
+			labelPreviousNotLoad.setVisible(false);
+			createScheduleButton.setVisible(true);
+		}
+	}
+	
 	@FXML
 	private void getFile() {
-
-		fileChooser.setTitle("Open Resource File");
-		filePath = fileChooser.showOpenDialog(new Stage());
-		fileChosen.setText(filePath.getName());
-		filename = filePath.getAbsolutePath();
-		extension = filename.substring(filename.lastIndexOf(".")+1).toLowerCase();
-
-		if(extension.equals("csv")) {
-			uploadFileCSV.setVisible(true);
-
-		}else if(extension.equals("json")){
-			uploadFileJSON.setVisible(true);
-
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.getExtensionFilters().addAll(new ExtensionFilter("CSV", "*.csv", "JSON", "*.json"));
+		File file = fileChooser.showOpenDialog(new Stage());
+		if (file == null) {
+			JOptionPane.showMessageDialog(null, "Por favor, selecione um ficheiro.", TYPE_MESSAGE,
+					JOptionPane.INFORMATION_MESSAGE);
+			return;
+		}
+		fileChosen.setText(file.getName());
+		String filePath = file.getAbsolutePath();
+		String fileExtension = Schedule.getFileExtension(filePath);	
+		if(".csv".equals(fileExtension) || ".json".equals(fileExtension)) {
+			fileChosen.setText(filePath);
+			uploadFileButton.setVisible(true);
 		}else {
-			JOptionPane.showMessageDialog(null, "O ficheiro importado tem extensão: "+ extension + "! Apenas são aceites extensões .json ou .csv", "Alerta" , JOptionPane.INFORMATION_MESSAGE);
+			JOptionPane.showMessageDialog(null, "O ficheiro importado tem extensão: " + fileExtension 
+					+ "! Apenas são aceites extensões .json ou .csv", TYPE_MESSAGE, JOptionPane.INFORMATION_MESSAGE);
 		}
 	}
-
-
+	
 	@FXML
-	private void saveFileCSV() {
-
-		fileChooser.getExtensionFilters().addAll(new ExtensionFilter("CSV", ".csv"));
-		filePathToSave = fileChooser.showSaveDialog(new Stage());
-		filenameToSave = filePathToSave.getAbsolutePath();
-
-		try {
-			Schedule.saveToCSV(scheduleUploaded, filenameToSave);
-		} catch (IOException e) {
-			JOptionPane.showMessageDialog(null,"Deu cócó ao gravar", "Alerta" , JOptionPane.ERROR_MESSAGE);
-		}
+	private void dealWithText() {
+		uploadFileButton.setVisible(true);
 	}
-
+	
 	@FXML
-	private void saveFileJSON() {
-
-		fileChooser.getExtensionFilters().addAll(new ExtensionFilter("JSON", ".json"));
-		filePathToSave = fileChooser.showSaveDialog(new Stage());
-		filenameToSave = filePathToSave.getAbsolutePath();
-
+	private void uploadFile() {
+		Schedule scheduleUploaded = null;
 		try {
-			Schedule.saveToJSON(scheduleUploaded, filenameToSave);
-		} catch (IOException e) {
-			JOptionPane.showMessageDialog(null,"Deu cócó ao gravar", "Alerta" , JOptionPane.ERROR_MESSAGE);
-		}
-	}
-
-
-	@FXML
-	private void uploadFileCSV() {
-
-		try {
-			scheduleUploaded = Schedule.loadCSV(filename);
-			viewSchedule.setVisible(true);
-			saveFileJSON.setVisible(true);
-
+			if (optionLocal.isSelected()){
+				scheduleUploaded = Schedule.callLoad(fileChosen.getText());
+				JOptionPane.showMessageDialog(null, "Horário importado com sucesso", TYPE_MESSAGE, JOptionPane.INFORMATION_MESSAGE);
+				createScheduleButton.setVisible(true);
+				
+			// NÃO FOI TESTADO - Se não for possível testar, na minha opinião, esta funcionalidade deve ser retirada
+			// do trabalho. Prefiro entregar um trabalho incompleto do que com uma funcionalidade que não foi testada
+			// para um teste básico
+			} else if (optionOnline.isSelected()) {
+				String filePath = urlChosen.getText();
+				String fileExtension = Schedule.getFileExtension(filePath);	
+				if (filePath.isBlank() || (!".csv".equals(fileExtension) && !".json".equals(fileExtension))) {
+					JOptionPane.showMessageDialog(null, "URL do ficheiro remoto inválido", TYPE_MESSAGE, 
+							JOptionPane.INFORMATION_MESSAGE);
+					return;
+				}
+				String tmpUrl= Schedule.downloadFileFromURL(filePath);
+				scheduleUploaded = Schedule.callLoad(tmpUrl);
+				createScheduleButton.setVisible(true);
+			
+			} else {
+				String filePath = webcalChosen.getText();
+				if (filePath.isBlank()) {
+					JOptionPane.showMessageDialog(null, "Link do calendário pessoal inválido", TYPE_MESSAGE, 
+							JOptionPane.INFORMATION_MESSAGE);
+					return;
+				}
+				scheduleUploaded = Schedule.loadWebcal(filePath);
+				createScheduleButton.setVisible(true);
+				
+			}
+			
 		}catch(Exception e1) {
-
-			JOptionPane.showMessageDialog(null, "Erro ao importar ficheiro CSV", "Alerta" , JOptionPane.INFORMATION_MESSAGE);
+			e1.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Erro ao importar ficheiro", TYPE_MESSAGE, JOptionPane.INFORMATION_MESSAGE);
 		}
 
 		App.SCHEDULE = scheduleUploaded;
 	}
-
-
+	
 	@FXML
-	private void uploadFileJSON() {
-
-		try {
-			scheduleUploaded = Schedule.loadJSON(filename);
-			viewSchedule.setVisible(true);
-			saveFileCSV.setVisible(true);
-		}catch(Exception e1) {
-			JOptionPane.showMessageDialog(null, "Erro ao importar ficheiro JSON", "Alerta" , JOptionPane.INFORMATION_MESSAGE);
-		}
-		App.SCHEDULE = scheduleUploaded;
+	private void createSchedule() throws IOException {
+		App.setRoot("/fxml/CreateSchedule");
 	}
-
 
 	@FXML
 	private void returnHome() {
@@ -126,11 +214,19 @@ public class ControllerLoadFileBeforeCreateSchedule implements Initializable{
 		}
 	}
 
-	//  ######################### MAIN #################################################### //
-
-
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		App.setStageSize(window.getPrefWidth(),window.getPrefHeight());
 	}
+	
+	// Método auxiliar
+	private static boolean scheduleIsEmpty(Schedule sdl) {
+		String empty = "Unknown Student Name\nUnknown Student Number\nSchedule is empty";
+		if(empty.endsWith(sdl.toString())) {
+			return true;
+		}
+		return false;
+	}
+	
+	
 }
--- ES-2023-2Sem-Terca-Feira-LEIPL-GrupoA/src/main/java/alfdozen/es_2023_2sem_terca_teira_leipl_grupoa/Main.java
+++ ES-2023-2Sem-Terca-Feira-LEIPL-GrupoA/src/main/java/alfdozen/es_2023_2sem_terca_teira_leipl_grupoa/Main.java
@@ -6,24 +6,31 @@ import java.util.Set;
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
-		Schedule scd = Schedule.loadCSV("./src/main/resources/horario_exemplo_completo.csv");
-		Set<String> uc = scd.getUniqueLecturesCourses();
-		String s = "[Seminário de Projecto I (Piudhist), Teoria dos Jogos e dos Contratos, Gestão de Conflitos, Cálculo I, Competências Académicas I, Fundamentos de Arquitectura de Computadores, Investimentos II]";
-		if(s.equals((String) uc.toString())) {
-			System.out.println(true);
-		} else {
-			System.out.println(false);
-		}
+//		Schedule scd = Schedule.loadCSV("./src/main/resources/horario_exemplo_completo.csv");
+//		Set<String> uc = scd.getUniqueLecturesCourses();
+//		String s = "[Seminário de Projecto I (Piudhist), Teoria dos Jogos e dos Contratos, Gestão de Conflitos, Cálculo I, Competências Académicas I, Fundamentos de Arquitectura de Computadores, Investimentos II]";
+//		if(s.equals((String) uc.toString())) {
+//			System.out.println(true);
+//		} else {
+//			System.out.println(false);
+//		}
+//		
+//		
+//		scd = new Schedule();
+//		uc = scd.getUniqueLecturesCourses();
+//		s = "[]";
+//		if(s.equals((String) uc.toString())) {
+//			System.out.println(true);
+//		} else {
+//			System.out.println(false);
+//		}
 		
+//		String uri = "webcal://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=carfa2@iscte.pt&password=G39uVMI8jjWvt1HwyiqMBA1PeogZBT4vwQu7PMHxxpj2w8y0GbKvETn09fI5e6aSL4HdfMiWwDxfO8wTcUK4SccLmXkc1c7z2lAFFB9JXgsS1Frinjt4zaHClWv3BhOS";
+//		Schedule s = Schedule.loadWebcal(uri);
+//		System.out.println(s.toString());
 		
-		scd = new Schedule();
-		uc = scd.getUniqueLecturesCourses();
-		s = "[]";
-		if(s.equals((String) uc.toString())) {
-			System.out.println(true);
-		} else {
-			System.out.println(false);
-		}
+		Schedule SCHEDULE = new Schedule();
+		System.out.println(SCHEDULE);
 		
 	}
 
