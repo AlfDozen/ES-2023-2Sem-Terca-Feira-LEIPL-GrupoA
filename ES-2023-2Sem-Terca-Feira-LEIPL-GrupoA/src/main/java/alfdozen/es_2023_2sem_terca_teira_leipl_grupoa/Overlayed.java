dcfc9d840404f77caf145748cc20fcc7e91492a7
Vaiking19 <vitor_hugo@iscte-iul.pt>
2023-05-07 18:37:42 +0100

    9 - Corrigir alterações sugeridos no code review
    Corrigidas as correções indicadas no codeReview e feitos os javadocs.

--- ES-2023-2Sem-Terca-Feira-LEIPL-GrupoA/src/main/java/alfdozen/es_2023_2sem_terca_teira_leipl_grupoa/Overlayed.java
+++ ES-2023-2Sem-Terca-Feira-LEIPL-GrupoA/src/main/java/alfdozen/es_2023_2sem_terca_teira_leipl_grupoa/Overlayed.java
@@ -1,3 +1,11 @@
+/**
+ * The Overlayed class is a JavaFX controller that manages the GUI components and events in a scene that displays
+ * information about overlapping and overcrowded lectures in a schedule. It provides two ListViews to display overlapping
+ * and overcrowded lectures and has methods to change scenes and initialize the contents of the ListViews.
+ * Implements the Initializable interface to initialize the contents of the ListViews when the corresponding FXML file is loaded.
+ * @author alfdozen
+ * @version 1.0.0
+ */
 package alfdozen.es_2023_2sem_terca_teira_leipl_grupoa;
 
 import java.io.IOException;
@@ -6,7 +14,6 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 
@@ -17,11 +24,14 @@ public class Overlayed implements Initializable{
 
 
 	@FXML
-	private ListView<Label> overlayedList;
+	private ListView<String> overlayedList;
 
 	@FXML
-	private ListView<Label> overlappedList;
+	private ListView<String> overlappedList;
 	
+    /**
+     * Changes the scene to the ViewSchedule scene when the createSchedule button is clicked.
+     */
 	@FXML
 	private void createSchedule() {
 		try {
@@ -31,7 +41,16 @@ public class Overlayed implements Initializable{
 		}
 	}
 
-
+	  /**
+     * Initializes the controller class. This method is automatically called after the FXML file has been loaded.
+     * It clears the contents of both ListView objects and checks if there are any overlapping lectures or overcrowded
+     * lectures in the current schedule. If there are overlapping lectures, it adds them to the "overlayedList"
+     * ListView. If there are overcrowded lectures, it adds them to the "overlappedList" ListView. If either ListView
+     * is empty, it adds a message indicating that there are no overlapping or overcrowded lectures.
+     *
+     * @param arg0 The location used to resolve relative paths for the root object or null if the location is not known.
+     * @param arg1 The resources used to localize the root object or null if the root object was not localized.
+     */
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 
@@ -39,32 +58,38 @@ public class Overlayed implements Initializable{
 		overlappedList.getItems().clear();
 
 		if(!App.getSchedule().hasOverlappingLectures()) {
-			Label noOver = new Label("Não há sobreposições de aulas");
+			String noOver = "Não há sobreposições de aulas";
 			overlayedList.getItems().add(noOver);
 		}else {
 			int n = App.getSchedule().getLectures().size();
 			for (int i = 0; i < n; i++) {
 				for (int j = i + 1; j < n; j++) {
-					if (App.getSchedule().getLectures().get(i).getTimeSlot().overlaps(App.getSchedule().getLectures().get(j).getTimeSlot())) {
-						Label over = new Label(App.getSchedule().getLectures().get(i).toString());
-						if(!overlayedList.getItems().contains(over)) {
-							overlayedList.getItems().add(over);
-						}
-					}
-					
-					if(App.getSchedule().getLectures().get(i).isOvercrowded()) {
-						Label over = new Label(App.getSchedule().getLectures().get(i).toString() + " - Sobrelotada");
-						if(!overlappedList.getItems().contains(over)) {
-							overlappedList.getItems().add(over);
-						}
-					}
+					Lecture lec1 = App.getSchedule().getLectures().get(i);
+					Lecture lec2 = App.getSchedule().getLectures().get(j);
+					addLectureToOverlayedAndOverlapped(lec1,lec2);
 				}
 			}
 		}
 		
 		if(overlappedList.getItems().isEmpty()) {
-			Label noOver = new Label("Não há aulas sobrelotadas");
+			String noOver = "Não há aulas sobrelotadas";
 			overlappedList.getItems().add(noOver);
+		}
+	}
+	
+	private void addLectureToOverlayedAndOverlapped(Lecture lec1, Lecture lec2){
+		if (lec1.getTimeSlot().overlaps(lec2.getTimeSlot())) {
+			String over = lec1.toString();
+			if(!overlayedList.getItems().contains(over)) {
+				overlayedList.getItems().add(over);
+			}
+		}
+		
+		if(lec1.isOvercrowded()) {
+			String over = lec1.toString() + " - Sobrelotada";
+			if(!overlappedList.getItems().contains(over)) {
+				overlappedList.getItems().add(over);
+			}
 		}
 	}
 }
--- ES-2023-2Sem-Terca-Feira-LEIPL-GrupoA/src/main/java/alfdozen/es_2023_2sem_terca_teira_leipl_grupoa/ViewSchedule.java
+++ ES-2023-2Sem-Terca-Feira-LEIPL-GrupoA/src/main/java/alfdozen/es_2023_2sem_terca_teira_leipl_grupoa/ViewSchedule.java
@@ -1,3 +1,13 @@
+/**
+ * The ViewSchedule class is the controller for the ViewSchedule.fxml file. It
+ * manages the GUI components and events in the ViewSchedule scene. The
+ * corresponding GUI is used to display the current schedule as a calendar view,
+ * save the schedule as a CSV or JSON file, return to the Main menu, create a new
+ * schedule or view any schedule conflicts.
+ * 
+ * @author alfdozen
+ * @version 1.0.0
+ */
 package alfdozen.es_2023_2sem_terca_teira_leipl_grupoa;
 
 import java.io.File;
@@ -6,15 +16,12 @@ import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ResourceBundle;
-
 import javax.swing.JOptionPane;
-
 import com.calendarfx.model.Entry;
 import com.calendarfx.view.CalendarView;
 import com.calendarfx.model.Calendar;
 import com.calendarfx.model.Calendar.Style;
 import com.calendarfx.model.CalendarSource;
-
 import javafx.application.Platform;
 import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
@@ -28,12 +35,15 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.FileChooser.ExtensionFilter;
 
-
-public class ViewSchedule implements Initializable{
-
-	private String[] daysOfWeek = {"Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado", "Domingo"};
+public class ViewSchedule implements Initializable {
 
 	public static final int NUM_COLUMNS = 7;
+	public static final int SLEEP_TIME = 10000;
+	public static final int WIDTH = 700;
+	public static final int HEIGHT = 750;
+	static final String ALERT_MESSAGE = "Alerta";
+	static final String ERROR_MESSAGE = "Erro";
+	static final String POP_UP_MESSAGE = "Erro ao guardar o ficheiro";
 
 	private FileChooser fileChooserToSave;
 	private File filePathToSave;
@@ -45,70 +55,93 @@ public class ViewSchedule implements Initializable{
 	@FXML
 	private CalendarView calendarView;
 
-
-
+	/**
+	 * This method is called by the event of clicking on the cancelButton. It
+	 * returns to the main scene.
+	 */
 	@FXML
 	private void returnHome() {
 		try {
 			App.setRoot("/fxml/Main");
 		} catch (IOException e) {
-			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Erro ao voltar para o menu principal", ERROR_MESSAGE,
+					JOptionPane.ERROR_MESSAGE);
 		}
 	}
 
+	/**
+	 * Event handler for saving the schedule to a CSV file. Shows a file chooser
+	 * dialog to select the save location. Saves the schedule to the selected file
+	 * in CSV format. Displays a success message with the file path if the save is
+	 * successful. Displays an error message if there is an exception while saving.
+	 */
 	@FXML
-	private void saveCSV() {
-		//PARA GRAVAR
+	private void saveFileCSV() {
+		// PARA GRAVAR
 		fileChooserToSave = new FileChooser();
 		fileChooserToSave.getExtensionFilters().addAll(new ExtensionFilter("CSV", ".CSV"));
 		filePathToSave = fileChooserToSave.showSaveDialog(new Stage());
-		
 
 		try {
-			if(filePathToSave != null) {
+			if (filePathToSave != null) {
 				filenameToSave = filePathToSave.getAbsolutePath();
 				Schedule.saveToCSV(App.getSchedule(), filenameToSave);
 			}
 		} catch (IOException e) {
-			JOptionPane.showMessageDialog(null,"Deu cócó ao gravar", "Alerta" , JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null, POP_UP_MESSAGE, ALERT_MESSAGE, JOptionPane.ERROR_MESSAGE);
 		}
 	}
 
+	/**
+	 * Event handler for saving the schedule to a JSON file. Shows a file chooser
+	 * dialog to select the save location. Saves the schedule to the selected file
+	 * in JSON format. Displays a success message with the file path if the save is
+	 * successful. Displays an error message if there is an exception while saving.
+	 */
 	@FXML
-	private void saveJSON() {
-		//PARA GRAVAR
+	private void saveFileJSON() {
+		// PARA GRAVAR
 		fileChooserToSave = new FileChooser();
 		fileChooserToSave.getExtensionFilters().addAll(new ExtensionFilter("JSON", ".json"));
 		filePathToSave = fileChooserToSave.showSaveDialog(new Stage());
-		
 
 		try {
-			if(filePathToSave != null) {
+			if (filePathToSave != null) {
 				filenameToSave = filePathToSave.getAbsolutePath();
 				Schedule.saveToJSON(App.getSchedule(), filenameToSave);
 			}
 
 		} catch (IOException e) {
-			JOptionPane.showMessageDialog(null,"Deu cócó ao gravar", "Alerta" , JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null, POP_UP_MESSAGE, ALERT_MESSAGE, JOptionPane.ERROR_MESSAGE);
 		}
 	}
 
+	/**
+	 * Loads the "CreateSchedule" scene when the "Create Schedule" button is
+	 * pressed.
+	 */
 	@FXML
 	private void createSchedule() {
 		try {
 			App.setRoot("/fxml/CreateSchedule");
 		} catch (IOException e) {
-			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Erro ao abrir o menu de criar horário", ERROR_MESSAGE,
+					JOptionPane.ERROR_MESSAGE);
 		}
 	}
 
+	/**
+	 * Shows a new stage with a list of all the lecture conflicts in the schedule.
+	 * The conflicts are displayed using the "Overlayed.fxml" file. If there is an
+	 * IO exception while loading the FXML file, no action is taken.
+	 */
 	@FXML
 	private void conflicts() {
 		try {
 
 			FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/Overlayed.fxml"));
 			Parent root = fxmlLoader.load();
-			Scene scene = new Scene(root, 700, 750);
+			Scene scene = new Scene(root, WIDTH, HEIGHT);
 
 			Stage newStage = new Stage();
 
@@ -117,16 +150,19 @@ public class ViewSchedule implements Initializable{
 			newStage.setResizable(false);
 			newStage.centerOnScreen();
 			newStage.show();
-			
 
 		} catch (IOException e) {
-			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Erro ao abrir o menu de mostrar conflitos do horário", ERROR_MESSAGE,
+					JOptionPane.ERROR_MESSAGE);
 		}
 	}
 
-	//  ######################### APOIO CALCULO #################################################### //
-
-
+	/**
+	 * Populates the calendar view with the entries of the lectures in the current
+	 * schedule. Creates a new calendar source, adds the lecture entries to it and
+	 * sets it as the calendar source for the calendar view. Also starts a thread to
+	 * update the current time on the calendar every 10 seconds.
+	 */
 	private void setLecturesEntries() {
 
 		try {
@@ -134,9 +170,9 @@ public class ViewSchedule implements Initializable{
 			Calendar<Lecture> iscte = new Calendar<>("ISCTE");
 			iscte.setStyle(Style.STYLE1);
 
-			for(Lecture lec : App.getSchedule().getLectures()) {
+			for (Lecture lec : App.getSchedule().getLectures()) {
 
-				if(lec.getTimeSlot().getDate() != null){
+				if (lec.getTimeSlot().getDate() != null) {
 
 					Entry<Lecture> aulas = new Entry<>();
 
@@ -168,10 +204,9 @@ public class ViewSchedule implements Initializable{
 						});
 
 						try {
-							// update every 10 seconds
-							sleep(10000);
+							sleep(SLEEP_TIME);
 						} catch (InterruptedException e) {
-							e.printStackTrace();
+							interrupt();
 						}
 
 					}
@@ -183,34 +218,39 @@ public class ViewSchedule implements Initializable{
 			updateTimeThread.start();
 
 		} catch (Exception e) {
-			System.out.println("caçei excepção");
+			JOptionPane.showMessageDialog(null, "Erro ao definir e mostrar horário", ERROR_MESSAGE,
+					JOptionPane.ERROR_MESSAGE);
 		}
 
 	}
 
-
-	//  ######################### INITIALIZE #################################################### //
-
-
+	/**
+	 * Initializes the controller class. This method is automatically called after
+	 * the FXML file has been loaded. It sets the stage size and title, enables
+	 * timezone support, and hides the source view. It also adds a listener to the
+	 * calendar sources, which hides the "Show Calendars" button if it exists.
+	 * Finally, it calls the setLecturesEntries method to populate the calendar view
+	 * with lectures.
+	 * 
+	 * @param arg0 the URL to the FXML file
+	 * @param arg1 the resource bundle associated with the FXML file
+	 */
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		App.setStageSize(calendarView.getPrefWidth(),calendarView.getPrefHeight());
+		App.setStageSize(calendarView.getPrefWidth(), calendarView.getPrefHeight());
 		App.getStage().setTitle("Calendar");
 		App.getStage().centerOnScreen();
 		calendarView.setEnableTimeZoneSupport(true);
 
 		calendarView.setShowAddCalendarButton(false);
 		calendarView.getSourceView().setVisible(false);
-		
-		 calendarView.getCalendarSources().addListener((ListChangeListener<CalendarSource>) change -> {
-		        // Get a reference to the ToggleButton and hide it
-		        ToggleButton toggleButton = (ToggleButton) calendarView.lookup(".calendar-show-calendars-button");
-		        if (toggleButton != null) {
-		            toggleButton.setVisible(false);
-		        }
-		    });
-		
-		setLecturesEntries();
 
+		calendarView.getCalendarSources().addListener((ListChangeListener<CalendarSource>) change -> {
+			ToggleButton toggleButton = (ToggleButton) calendarView.lookup(".calendar-show-calendars-button");
+			if (toggleButton != null) {
+				toggleButton.setVisible(false);
+			}
+		});
+		setLecturesEntries();
 	}
 }
